a6d5a9ab9933be25e1c86ba9986f2e6cae87d16d
aw3s0me <akorovin00@gmail.com>
2016-12-10 00:40:09 +0100

    fetching modifier class name

--- src/main/java/elements/modifiers/ModifierClass.java
+++ src/main/java/elements/modifiers/ModifierClass.java
@@ -1,17 +1,20 @@
 package elements.modifiers;
 
 import elements.base.ClamlBase;
+import elements.base.LabelBase;
 import org.w3c.dom.Element;
+
+import javax.xml.xpath.XPathExpressionException;
 
 /**
  * Created by akorovin on 09.12.2016.
  */
-public class ModifierClass extends ClamlBase {
+public class ModifierClass extends LabelBase {
     // initial modifier code
     protected String isPartOf;
 
-    public ModifierClass(Element xmlNode) {
-        super(xmlNode);
+    public ModifierClass(Element xmlNode) throws XPathExpressionException {
+        super(xmlNode, String.format("/ClaML/ModifierClass[@modifier='%1$s'", xmlNode.getAttribute("modifier")) + " and @code='%1$s']");
         this.isPartOf = xmlNode.getAttribute("modifier");
     }
 
