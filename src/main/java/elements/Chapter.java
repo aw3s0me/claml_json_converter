3347039cb1664c12bbab83edd349b5070fd3ac3b
Aleksandr Korovin <Aleksandr Korovin>
2016-12-09 16:26:55 +0100

    convert chapter to json

--- src/main/java/elements/Chapter.java
+++ src/main/java/elements/Chapter.java
@@ -1,5 +1,6 @@
 package elements;
 
+import org.json.simple.JSONObject;
 import org.w3c.dom.Element;
 
 import javax.xml.xpath.XPathExpressionException;
@@ -18,5 +19,13 @@ public class Chapter extends ClassKind {
                 "name='" + name + '\'' +
                 ", code='" + code + '\'' +
                 '}';
+    }
+
+    public JSONObject toJSON() {
+        JSONObject obj = new JSONObject();
+        obj.put("code", this.getCode());
+        obj.put("name", this.getName());
+        obj.put("hasPart", this.getHasPart());
+        return obj;
     }
 }
