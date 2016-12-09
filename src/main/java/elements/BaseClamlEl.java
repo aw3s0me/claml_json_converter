63fee0fef504c16250054609523d3196d1701e65
Aleksandr Korovin <Aleksandr Korovin>
2016-12-09 14:09:48 +0100

    refactoring

--- src/main/java/elements/BaseClamlEl.java
+++ src/main/java/elements/BaseClamlEl.java
@@ -7,6 +7,10 @@ import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by korovin on 12/9/2016.
@@ -15,17 +19,31 @@ public class BaseClamlEl {
     protected XPath xpath = XPathFactory.newInstance().newXPath();
     protected String name;
     protected String code;
+    protected Map<String, BaseClamlEl> children = new HashMap<String, BaseClamlEl>();
+    protected List<String> childrenCodes = new ArrayList<String>();
+    protected String basePath;
+
+    public BaseClamlEl(Element xmlNode, String basePath)  throws XPathExpressionException {
+        // map chapter
+        this.code = xmlNode.getAttribute("code");
+        this.basePath = String.format(basePath, this.code);
+        this.name = this.fetchName(xmlNode);
+    }
 
     /**
      * create xpath and fetch name from Rubric with attr value preferred
      * @param element
-     * @param path
      * @return
      * @throws XPathExpressionException
      */
-    protected String fetchName(Element element, String path) throws XPathExpressionException {
+    protected String fetchName(Element element) throws XPathExpressionException {
+        String path = this.basePath + "/Rubric[@kind='preferred']/Label";
         Node node = (Node) xpath.compile(path).evaluate(element, XPathConstants.NODE);
 
         return node.getTextContent();
+    }
+
+    protected List<String> fetchChildrenCodes(Element element, String path) throws XPathExpressionException {
+        return null;
     }
 }
