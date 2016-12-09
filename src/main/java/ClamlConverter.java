ff714837f8877a2e907badff3dcabe8c8d20bc59
aw3s0me <akorovin00@gmail.com>
2016-12-09 12:36:29 +0100

    adding xpath

--- src/main/java/ClamlConverter.java
+++ src/main/java/ClamlConverter.java
@@ -3,6 +3,8 @@ import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathFactory;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -12,14 +14,17 @@ import java.util.Map;
  */
 public class ClamlConverter implements IClamlConverter {
     private Document xmlData;
+    private XPath xpath;
     private Map<String, Chapter> chapters = new HashMap<String, Chapter>();
 
     public ClamlConverter(Document xmlData) {
         this.xmlData = xmlData;
+        XPathFactory factory = XPathFactory.newInstance();
+        this.xpath = factory.newXPath();
     }
 
     public void convertToJson() {
-
+        //
     }
 
     private Map<String, Chapter> getChapters() {
@@ -29,6 +34,6 @@ public class ClamlConverter implements IClamlConverter {
     public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
         FileLoader loader = new FileLoader();
         Document dom = loader.getDom("icd.claml.xml");
-        System.out.println(dom);
+        System.out.println(dom.getDocumentElement());
     }
 }
