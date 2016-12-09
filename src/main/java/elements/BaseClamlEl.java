46f86a6323391a7fb031b8cb66e6ccf6cec714ca
aw3s0me <akorovin00@gmail.com>
2016-12-09 13:18:14 +0100

    fetched all chapters with their codes

--- src/main/java/elements/BaseClamlEl.java
+++ src/main/java/elements/BaseClamlEl.java
@@ -1,8 +1,11 @@
 package elements;
 
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 
 import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
 /**
@@ -11,8 +14,16 @@ import javax.xml.xpath.XPathFactory;
 public class BaseClamlEl {
     protected XPath xpath = XPathFactory.newInstance().newXPath();
 
-    protected String fetchName(Element element) {
-        // TODO: create xpath and fetch name from Rubric with attr value preferred
-        return "";
+    /**
+     * create xpath and fetch name from Rubric with attr value preferred
+     * @param element
+     * @param path
+     * @return
+     * @throws XPathExpressionException
+     */
+    protected String fetchName(Element element, String path) throws XPathExpressionException {
+        Node node = (Node) xpath.compile(path).evaluate(element, XPathConstants.NODE);
+
+        return node.getTextContent();
     }
 }
