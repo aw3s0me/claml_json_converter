1bfcc4b109a527a968221eb74ad85f8bccfe8614
Aleksandr Korovin <Aleksandr Korovin>
2016-12-09 12:58:23 +0100

    getting chapters

--- src/main/java/elements/Chapter.java
+++ src/main/java/elements/Chapter.java
@@ -1,12 +1,25 @@
 package elements;
 
-import org.w3c.dom.Node;
+import org.w3c.dom.Element;
 
 /**
  * Created by korovin on 12/9/2016.
  */
 public class Chapter {
-    public Chapter(Node xmlNode) {
+    private String name;
+    private String code;
+
+    public Chapter(Element xmlNode) {
         // TODO: map chapter
+        this.name = "test";
+        this.code = "I";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCode() {
+        return code;
     }
 }
